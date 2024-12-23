88f02c3c677e82f1068c5980b60c2348b516e49e
daviddelcerro <daviddelcerrod@gmail.com>
2024-12-23 12:29:57 +0100

    Post and morgan

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,5 +1,12 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
+
+morgan.token('body', (req) => JSON.stringify(req.body));
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+
+app.use(express.json())
 
 let persons = [
     { 
@@ -29,6 +36,58 @@ app.get('/', (req, res) => {
 })
 app.get('/api/persons', (req, res) => {
   res.json(persons)
+})
+
+app.get('/info', (req, res) => {
+  const date = new Date()
+  res.send(`<p>Phonebook has info for ${persons.length} people</p>
+            <p>${date}</p>`)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(person => person.id === id)
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()   
+  }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(person => person.id !== id)
+  res.status(204).end()
+})
+
+const generatedId = () => {
+  const id = Math.floor(Math.random() * 100000)
+    return id
+}
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+
+  if(!body.name || !body.number) {
+    return res.status(400).json({
+        error: 'content missing'
+    })
+  }
+
+  if(persons.some(person => person.name === body.name)) {
+    return res.status(400).json({
+        error: 'name must be unique'
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generatedId()
+  }
+
+  persons = persons.concat(person)
+  res.json(person)
 })
 
 
