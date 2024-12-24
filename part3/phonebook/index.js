b96520e4ac468b911ca9a0bc0da44a9bf98c1312
daviddelcerro <daviddelcerrod@gmail.com>
2024-12-24 11:51:01 +0100

    Update person.js connection Backend to mongo

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
+require('dotenv').config()
 const morgan = require('morgan')
-const cors = require('cors')
+
+const Person = require('./models/person')
+
+app.use(express.static('dist'))
 
 morgan.token('body', (req) => JSON.stringify(req.body));
 
@@ -9,38 +13,24 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 
 app.use(express.json())
 
-app.use(express.static('dist'))
-
+const cors = require('cors')
 app.use(cors())
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'unknown endpoint' })
+}
+
+
+let persons = []
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
 })
+
 app.get('/api/persons', (req, res) => {
-  res.json(persons)
+  Person.find({}).then(persons => {
+    res.json(persons)
+  })
 })
 
 app.get('/info', (req, res) => {
@@ -52,11 +42,9 @@ app.get('/info', (req, res) => {
 app.get('/api/persons/:id', (req, res) => {
   const id = Number(req.params.id)
   const person = persons.find(person => person.id === id)
-  if (person) {
+  Person.findById(id).then(person => {
     res.json(person)
-  } else {
-    res.status(404).end()   
-  }
+  })
 })
 
 app.delete('/api/persons/:id', (req, res) => {
@@ -95,7 +83,8 @@ app.post('/api/persons', (req, res) => {
   res.json(person)
 })
 
+app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
