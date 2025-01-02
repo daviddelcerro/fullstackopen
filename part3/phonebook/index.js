16ef1f5c35be691543665dc731bf453ede7ce168
daviddelcerro <daviddelcerrod@gmail.com>
2025-01-02 12:07:40 +0100

    Updated errors

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -18,6 +18,8 @@ const errorHandler = (error, req, res, next) => {
     return res.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
     return res.status(400).json({ error: error.message })
+  } else if (error.phone === 'ValidationError') {
+    return res.status(400).json({ error: error.message })
   }
 
   next(error)
@@ -86,20 +88,18 @@ app.post('/api/persons', (req, res) => {
   person.save().then(savedPerson => {
     res.json(savedPerson)
   })
+  .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (req, res) => {
-  const body = req.body
+  const {name, number}= req.body
 
-  const person = {
-    name: body.name,
-    number: body.number,
-  }
 
-  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+  Person.findByIdAndUpdate(req.params.id, {name, number}, { new: true, runValidators: true, context: 'query' })
     .then(updatedPerson => {
       res.json(updatedPerson)
     })
+    .catch(error => next(error))
 })
 
 app.use(unknownEndpoint)
