cd37e8a7ec3b8368b97f9a120b08481e363b5566
daviddelcerro <daviddelcerrod@gmail.com>
2025-01-02 11:54:02 +0100

    Added validator

--- part3/phonebook/models/person.js
+++ part3/phonebook/models/person.js
@@ -15,8 +15,24 @@ mongoose.connect(url)
     })
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 3
+    },
+    number: {
+        type: String,
+        required: true,
+        minlength: 8,
+        validate : {
+            validator: function(v) {
+                return /\d{2}-\d{6}/.test(v)
+            },
+            message: props => `${props.value} is not a valid phone number!`
+        }
+
+    }
 })
 
 personSchema.set('toJSON', {
